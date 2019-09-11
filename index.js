cda8381afb68df57f77ca8f46c6a1293c78a97b5
Katie Hansen <khansen0415@gmail.com>
2019-09-11 12:48:01 -0400

    add functioning image slider

--- index.js
+++ index.js
@@ -1,15 +1,89 @@
+'use strict';
+
+//VARIABLES
 const toggleBtn = document.querySelector('.toggle-button');
 const collapse = document.querySelector('.collapse-content');
 const navLinks = document.querySelectorAll('.nav-link');
+const slides = document.querySelectorAll('.slide');
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const controls = document.querySelectorAll('.image-controls');
+const auto = true;
+const intervalTime = 5000;
+let slideInterval;
 
-toggleBtn.addEventListener('click', function(){
+
+
+//FUNCTIONS
+
+//toggle nav 
+function navToggle(){
     toggleBtn.classList.toggle('cross');
     collapse.classList.toggle('toggle');
-});
+}
+
+//hide nav
+function hideNav(){
+    collapse.classList.remove('toggle');
+    toggleBtn.classList.remove('cross');
+}
+
+
+//click next button to go to next slide
+function nextSlide(){
+    if(auto){
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+    const current = document.querySelector('.current');
+    current.classList.remove('current');
+    if (current.nextElementSibling) {
+        current.nextElementSibling.classList.add('current');
+    } else {
+       slides[0].classList.add('current'); 
+    }
+    setTimeout(() => current.classList.remove('current'), 200);
+}
+
+
+//click previous button to go to next slide
+function prevSlide(){
+    if(auto){
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+    const current = document.querySelector('.current');
+    current.classList.remove('current');
+    if (current.previousElementSibling) {
+        current.previousElementSibling.classList.add('current');
+    } else {
+       slides[slides.length - 1].classList.add('current'); 
+    }
+    setTimeout(() => current.classList.remove('current'), 200);
+}
+
+
+//auto slide on page load
+function autoSlide(){
+    if(auto) {
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+}
+
+
+
+//EVENT LISTENERS
+toggleBtn.addEventListener('click', navToggle);
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
 
 navLinks.forEach(link => {
-    link.addEventListener('click', function(){
-        collapse.classList.remove('toggle');
-        toggleBtn.classList.remove('cross');
-    });
+    link.addEventListener('click', hideNav);
 });
+
+
+
+
+
+//FUNCTION CALL
+autoSlide();
